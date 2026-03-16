c6a45a183dfa39e5cc1001edaa05499ccc6c9631
40329285 <rdonnelly49@qub.ac.uk>
2026-03-16 19:11:32 +0000

    compressor cli updated toi give numbered options

--- text-compressor/main.cpp
+++ text-compressor/main.cpp
@@ -1,28 +1,64 @@
 #include <iostream>
+#include <string>
+#include <filesystem>
 #include "huffman.h"
 
-int main(int argc, char* argv[])
+int main()
 {
-    if(argc < 4)
-    {
-        std::cout<<"Usage:\n";
-        std::cout<<"compressor compress input.txt output.huff\n";
-        std::cout<<"compressor decompress input.huff output.txt\n";
-        return 1;
-    }
-
-    std::string mode = argv[1];
-    std::string input = argv[2];
-    std::string output = argv[3];
-
     Huffman h;
 
-    if(mode == "compress")
-        h.compressFile(input,output);
+    while(true)
+    {
+        std::cout << "\n==== Huffman Compressor ====\n";
+        std::cout << "1. Compress file\n";
+        std::cout << "2. Decompress file\n";
+        std::cout << "3. Exit\n";
+        std::cout << "Enter choice: ";
 
-    else if(mode == "decompress")
-        h.decompressFile(input,output);
+        int choice;
+        std::cin >> choice;
 
-    else
-        std::cout<<"Unknown command\n";
+        if(choice == 1)
+        {
+            std::string input;
+
+            std::cout << "Enter input file: ";
+            std::cin >> input;
+
+            std::string output =
+                std::filesystem::path(input).stem().string() + ".huff";
+
+            h.compressFile(input, output);
+
+            std::cout << "Compressed file created: " << output << "\n";
+        }
+
+        else if(choice == 2)
+        {
+            std::string input;
+
+            std::cout << "Enter compressed file: ";
+            std::cin >> input;
+
+            std::string output =
+                std::filesystem::path(input).stem().string() + "_restored.txt";
+
+            h.decompressFile(input, output);
+
+            std::cout << "Restored file created: " << output << "\n";
+        }
+
+        else if(choice == 3)
+        {
+            std::cout << "Exiting program.\n";
+            break;
+        }
+
+        else
+        {
+            std::cout << "Invalid choice. Try again.\n";
+        }
+    }
+
+    return 0;
 }
